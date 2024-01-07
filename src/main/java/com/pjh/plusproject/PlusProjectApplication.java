10e3adc1050d1a74586c4e5e8079d4ae48e79e9a
parkjihwan <mankcom7708@gmail.com>
2024-01-07 16:16:15 +0900

    feat #35 : CI/CD를 위한 변경

--- src/main/java/com/pjh/plusproject/PlusProjectApplication.java
+++ src/main/java/com/pjh/plusproject/PlusProjectApplication.java
@@ -2,16 +2,23 @@ package com.pjh.plusproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @ConfigurationPropertiesScan
 @SpringBootApplication(scanBasePackages = "com.pjh.plusproject")
-public class PlusProjectApplication {
+public class PlusProjectApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PlusProjectApplication.class, args);
     }
 
+    // war 파일 배포를 위해 선언
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(PlusProjectApplication.class);
+    }
 }
